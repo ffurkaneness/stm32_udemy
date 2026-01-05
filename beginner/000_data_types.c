5ddab4f6d0a66209262070c2a32c59d4d5f2e33d
ffurkaneness <furkaneneskrbck@gmail.com>
2026-01-05 10:17:30 +0300

    Data type examples

---
+++ beginner/000_data_types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    unsigned char AB = 160;
+    unsigned char BC = 40;
+    unsigned char AC = AB + BC;
+    printf("Distance from point A to C is %u\n",AC);
+
+
+    return 0;
+}
---
+++ beginner/002_sizeof.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Size of (int) data type is: %d\n",sizeof(int));
+    printf("Size of (float) data type is: %d\n",sizeof(float));
+    printf("Size of (char) data type is: %d\n",sizeof(char));
+    printf("Size of (double) data type is: %d\n",sizeof(double));
+    printf("Size of (long) data type is: %d\n",sizeof(long));
+    printf("Size of (long long) data type is: %d\n",sizeof(long long));
+    printf("Size of (unsigned char) data type is: %d\n",sizeof(unsigned char));
+
+
+    return 0;
+}
